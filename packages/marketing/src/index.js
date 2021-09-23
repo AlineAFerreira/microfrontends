52c2e3433411e7d31381bbf29739a73e84d1ec44
Aline Ferreira <aline.a.ferreira@live.com>
2021-09-22 21:24:19 -0300

    Change test

--- packages/marketing/src/index.js
+++ packages/marketing/src/index.js
@@ -2,5 +2,5 @@
 This really gives webpack the opportunity to realize that before executing that bootstrapped JS file we have to go and fetch some code from the products project. */
 
 /*It is just to allow Webpack that opportunity to go and get some additional JavaScript and make sure that we've got the project code or the products code ready along with the code ready as well*/
-import('./bootstrap.js')
+import('./bootstrap.js');
 
